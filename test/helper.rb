ffd3fa5b10f86a404b6111104690aa311030d867
Nick Howard <ndh@baroquebobcat.com>
2010-02-07 23:05:16 -0600

    Initial commit to sinatra-twitter-oauth.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'sinatra-twitter-oauth'
+
+class Test::Unit::TestCase
+end
